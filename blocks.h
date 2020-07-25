68e236c99a0af5b664152a400f2a6d5c74dcba25
Peter K. Moss <peter@moss.dk>
2020-07-25 10:18:34 +0200

    Updated

--- blocks.h
+++ blocks.h
@@ -4,15 +4,15 @@ static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     {"📆", "date '+%a %d-%m-%y %T'", 60, 0},
 
-    {"🧠", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
 
     {"⌨", "setxkbmap -query | awk '/layout/{print $2}'", 120, 0},
 
-    {"🌐", "hostname -i | sed 's/ *$//g'", 0, 1},
+    {"", "hostname -i | sed 's/ *$//g'", 0, 1},
 
     {"🔊", "pamixer --get-volume-human | tr -d '%'", 0, 10},
 
-    {"🌡", "sensors | awk '/^temp1:/{print $2}'", 15, 0},
+    {"糖", "sensors | awk '/^Package/{print $4}'", 15, 0},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
