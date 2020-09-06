ef6fe9037d25dafac56ebf809ff80e6451aa53a1
Peter K. Moss <peter@moss.dk>
2020-09-06 10:40:11 +0200

    kernel version

--- blocks.h
+++ blocks.h
@@ -2,9 +2,16 @@
 // recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-    {"  ", "pacman -Qu | wc -l", 60, 0},
+    {"  ", "uname -r", 60, 0},
 
-    {"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 15, 0},
+    {"  ",
+     "df -lh | grep \"/dev/nvme\" | awk '{print $3 \"/\" $2 \" - \" $5 \" "
+     "used\"}'",
+     60, 0},
+
+    {"  ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 5, 0},
+
+    {"  ", "pacman -Qu | wc -l", 60, 0},
 
     {" ⌨ ", "setxkbmap -query | awk '/layout/{print $2}'", 120, 0},
 
@@ -12,9 +19,9 @@ static const Block blocks[] = {
 
     {" 🔊 ", "pamixer --get-volume-human | tr -d '%'", 0, 10},
 
-    {"  ", "sensors | awk '/^Package/{print $4}'", 15, 0},
+    {"  ", "sensors | awk '/^Package/{print $4}'", 5, 0},
 
-    {" 📆 ", "date '+%a %d-%m-%y %T'", 2, 0},
+    {" 📆 ", "date '+%d-%m %T'", 1, 0},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no
